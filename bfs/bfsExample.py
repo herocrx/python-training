7d0979384277bb356ecc0fe9810b69e06fe62fc6
Hubert Kuc <hubertkuc13@gmail.com>
2021-01-02 21:32:49 +0100

    Implementing simple bfs

---
+++ bfs/bfsExample.py
@@ -0,0 +1,50 @@
+
+
+from collections import deque
+
+class Node:
+    def __init__(self, number):
+        self.number = number
+        self.children = list()
+        
+
+    def addChild(self, n):
+        self.children.append(n)
+
+def bfs():
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+    n6 = Node(6)
+    n7 = Node(7)
+    n8 = Node(8)
+    n9 = Node(9)
+
+    n1.addChild(n2)
+    n1.addChild(n9)
+    n2.addChild(n3)
+    n3.addChild(n4)
+    n4.addChild(n5)
+    n5.addChild(n6)
+    n6.addChild(n7)
+    n7.addChild(n8)
+    n7.addChild(n1)
+    n8.addChild(n9)
+
+
+    visited = set()
+    deq = deque()
+    deq.append(n1)
+
+    while deq:
+
+        currentNode = deq.pop()
+        if currentNode not in visited:
+            print('Executing node: ', currentNode.number)
+            visited.add(currentNode)
+            for node in currentNode.children:
+                deq.append(node)
+
+bfs()
