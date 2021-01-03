363b8669887203fde4e32151a9e9a8dee422aeb3
Hubert Kuc <hubertkuc13@gmail.com>
2021-01-03 19:31:26 +0100

    Fun with gradients in PyTorch's tensors. Few examples how it behaves

---
+++ pytorch/assignment3.py
@@ -0,0 +1,23 @@
+import torch
+
+a = torch.tensor([2., 3.], requires_grad=True)
+b = torch.tensor([6., 4.], requires_grad=True)
+
+Q = 3*a**3 - b**2
+
+print(a)
+print(b)
+print(Q)
+external_grad = torch.tensor([1., 1.])
+Q.backward(gradient=external_grad)
+print('A grad:', a.grad)
+print('B grad:', b.grad)
+
+x = torch.rand(5, 5)
+y = torch.rand(5, 5)
+z = torch.rand((5, 5), requires_grad=True)
+
+a = x + y
+print(f"Does `a` require gradients? : {a.requires_grad}")
+b = x + z
+print(f"Does `b` require gradients?: {b.requires_grad}")
